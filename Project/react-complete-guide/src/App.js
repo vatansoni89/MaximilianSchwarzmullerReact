1ae0c135e3a1e9bd258f22a3c88220adb0b47dd8
Vatan Soni <vatansoni89@gmail.com>
2019-09-23 15:18:26 +0530

    replace state prop by useState hook

--- Project/react-complete-guide/src/App.js
+++ Project/react-complete-guide/src/App.js
@@ -1,50 +1,72 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person.js";
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setpersonsState] = useState({
     persons: [
       { name: "Vatan", age: 30 },
       { name: "Sonu", age: 28 },
       { name: "Raju", age: 26 }
-    ]
-  };
+    ],
+    otherState: "some other value"
+  });
 
-  switchNameHandler = () => {
-    // React don't respect this ===>>> this.state.persons[0].name = "Manuuu";
-    this.setState({
+  const [hii, funHii] = useState("hii111");
+  console.log(personsState, hii);
+
+  const switchNameHandler = () => {
+    // React don't respect this ===>>> personsState.persons[0].name = "Manuuu";
+    setpersonsState({
       persons: [
         { name: "Soni", age: 30 },
         { name: "Sonu", age: 28 },
         { name: "Raju", age: 26 }
-      ]
+      ],
+      otherState: personsState.otherState
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hii</h1>
-        <p>working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        >
-          My hobbie: Badminton
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hii</h1>
+      <p>working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      >
+        My hobbie: Badminton
+      </Person>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
-export default App;
+export default app;
+
+// state = {
+//   persons: [
+//     { name: "Vatan", age: 30 },
+//     { name: "Sonu", age: 28 },
+//     { name: "Raju", age: 26 }
+//   ]
+// };
+
+// switchNameHandler = () => {
+//   // React don't respect this ===>>> personsState.persons[0].name = "Manuuu";
+//   this.setState({
+//     persons: [
+//       { name: "Soni", age: 30 },
+//       { name: "Sonu", age: 28 },
+//       { name: "Raju", age: 26 }
+//     ]
+//   });
+// };
